1f699793e5c8796eac45916037f27a82e6f4c9a8
kenmark103 <kenmark103@gmail.com>
2024-10-16 17:51:03 +0300

    finished up on setting application properties

--- debtmanagement/backend/api-gateway-service/src/main/java/com/projects/api_gateway_service/middleware/JwtAndLicenseGatewayFilter.java
+++ debtmanagement/backend/api-gateway-service/src/main/java/com/projects/api_gateway_service/middleware/JwtAndLicenseGatewayFilter.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
+
+import com.projects.shared.components.JwtTokenProvider;
+
 import reactor.core.publisher.Mono;
 
 @Component
