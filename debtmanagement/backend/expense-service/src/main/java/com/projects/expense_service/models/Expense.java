c358675969070cfd244bad914a1786c3d4559062
kenmark103 <kenmark103@gmail.com>
2024-10-28 06:53:39 +0000

    modified:   debtmanagement/backend/expense-service/src/main/java/com/projects/expense_service/models/Expense.java

--- debtmanagement/backend/expense-service/src/main/java/com/projects/expense_service/models/Expense.java
+++ debtmanagement/backend/expense-service/src/main/java/com/projects/expense_service/models/Expense.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+
 @Entity
 public class Expense {
     @Id
